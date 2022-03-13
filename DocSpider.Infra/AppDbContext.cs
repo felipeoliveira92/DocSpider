96f6596cc6ef83d988fedcdd4959ce8473165101
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2022-03-13 12:19:47 -0300

    configurado o contexto e a string de conexao na statup

---
+++ DocSpider.Infra/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DocSpider.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocSpider.Infra
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Document> Documents { get; set; }
+    }
+}
--- DocSpider.WebApp/Startup.cs
+++ DocSpider.WebApp/Startup.cs
@@ -1,6 +1,8 @@
+using DocSpider.Infra;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,10 @@ namespace DocSpider.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetSection("ConnectionStrings:Default");
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString.Value));
+            services.AddScoped<AppDbContext, AppDbContext>();
+
             services.AddControllersWithViews();
         }
 
