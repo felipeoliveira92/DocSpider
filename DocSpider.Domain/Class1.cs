09d89c4e23c66d86aa80ac858b94048974828d38
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2022-03-13 11:56:17 -0300

    iniciado o projeto e a classe Document

---
+++ DocSpider.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DocSpider.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ DocSpider.Domain/Document.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DocSpider.Domain
+{
+    public class Document
+    {
+        public Document(string title, string description, byte file, string fileName)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.File = file;
+            this.FileName = fileName;
+            DateCreate = DateTime.Now;
+
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Titulo")]
+        [Required(ErrorMessage = "Campo Requerido!")]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [DisplayName("Descrição")]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        [DisplayName("Arquivo")]
+        public byte File { get; set; }
+
+        [DisplayName("Nome")]
+        public string FileName { get; set; }
+
+        [DisplayName("Data de Criação")]
+        public DateTime DateCreate { get; set; }
+
+    }
+}
